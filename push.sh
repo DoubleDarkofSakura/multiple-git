08ca10fba9206afaf752b44e681f4a55838d142c
moyinzi <632378816@qq.com>
2018-02-10 01:35:40 +0800

    精简shell

--- push.sh
+++ push.sh
@@ -2,6 +2,6 @@ git push -u origin --all
 git push coding --all
 git push gitee --all
 
-git push -u origin master --tags
-git push coding master --tags
-git push gitee master --tags
+git push -u origin --tags
+git push coding --tags
+git push gitee --tags
